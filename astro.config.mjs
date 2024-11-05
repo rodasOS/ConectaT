0c06670f32508371c8b138f7a5e71ac625bab335
marioorlandosalesrodas@gmail.com <marioorlandosalesrodas@gmail.com>
2024-11-05 11:02:55 -0600

    Revert "Agregando vercel analiticas"
    This reverts commit 24d90910ace5a53828d8b1471bab71440541c164.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import vercel from '@astrojs/vercel/serverless';
+// @ts-check
+import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
-	output: 'server',
-	adapter: vercel({
-		webAnalytics: { enabled: true },
-	}),
+  integrations: [tailwind()]
 });
