597e7b4391afca283558c2e9b752af8c05a255bb
marioorlandosalesrodas@gmail.com <marioorlandosalesrodas@gmail.com>
2024-11-06 11:33:14 -0600

    Agregando lista de cursos estatica

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
-/** @type {import('tailwindcss').Config} */
+//** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				nobel: '#b4b4b4',
+				'lucky-point': '#1c1464',
+				'green-vogue': '#042c54',
+				olivine: '#8cb884',
+				paradiso: '#307484',
+				'bay-of-many': '#302c88',
+				martinique: '#342c54',
+				madison: '#082c54',
+			},
+		},
 	},
 	plugins: [],
-}
+};
